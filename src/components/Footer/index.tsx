877aa781f36a4ce0a2dd0ae6d3b0f06a0ff719c1
Daniel <daniel.garcia@issivs.com>
2023-11-19 15:35:31 -0500

    Footer component created

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from "./styles.module.css";
+import Image from '../../../node_modules/next/image';
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <section className={styles.left}>
+                <ul>
+                <li><a href="">Daniel Garcia | Profesional Dev</a></li>
+                <li><a href="https://www.linkedin.com/in/daniel-antonio-garcia-arias-5661141b3/">Mi Linkedin</a></li>
+                <li><a href="https://github.com/garcia98daniel">Mi Git hub</a></li>
+                </ul>
+            </section>
+            <section className={styles.right}>
+                <Image src="/images/logo.svg" alt="Logo de Million and up 2023" layout="fill" objectFit="contain"/>
+            </section>
+        </footer>
+    );
+}
+
+export default Footer;
