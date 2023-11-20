5d44ec2dc3ee4ea1fd39f16ed310c1f5a0917c12
Daniel <daniel.garcia@issivs.com>
2023-11-20 12:19:25 -0500

    Interfaced created

---
+++ src/ts-types/custom.types.ts
@@ -0,0 +1,27 @@
+export interface ICurrencyState{
+  currencyReducer: {
+    requesting: boolean,
+    success: boolean,
+    error: string,
+    currency: Currency[],
+  }
+}
+
+export interface Currency {
+  id: string;
+  symbol: string;
+  name: string;
+  nameid: string;
+  rank: number;
+  price_usd: string;
+  percent_change_24h: string;
+  percent_change_1h: string;
+  percent_change_7d: string;
+  price_btc: string;
+  market_cap_usd: string;
+  volume24: number;
+  volume24a: number;
+  csupply: string;
+  tsupply: string;
+  msupply: string;
+}
